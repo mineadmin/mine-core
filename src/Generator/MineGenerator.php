ef0530db84d18d07baf061c70cf10e7251dc460c
X.Mo <root@imoi.cn>
2023-05-26 11:17:14 +0800

    fix: 修复改成package后，生成代码时找不到模板文件

--- src/Generator/MineGenerator.php
+++ src/Generator/MineGenerator.php
@@ -40,7 +40,7 @@ abstract class MineGenerator
      */
     public function __construct(ContainerInterface $container)
     {
-        $this->setStubDir(BASE_PATH . '/mine/Generator/Stubs/');
+        $this->setStubDir(BASE_PATH . '/vendor/xmo/mine-core/src/Generator/Stubs/');
         $this->container = $container;
     }
 
