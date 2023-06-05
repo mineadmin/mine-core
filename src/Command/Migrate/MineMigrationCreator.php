7330a4d66c683b390dba1669382da670a0ad8dc0
熊孝兵 <wisp-x@qq.com>
2023-06-05 09:26:15 +0800

    fix: 修复模块生成迁移时找不到模板文件的 bug

--- src/Command/Migrate/MineMigrationCreator.php
+++ src/Command/Migrate/MineMigrationCreator.php
@@ -19,6 +19,6 @@ class MineMigrationCreator extends MigrationCreator
 
     public function stubPath(): string
     {
-        return BASE_PATH . '/mine/Command/Migrate/Stubs';
+        return __DIR__ . '/Stubs';
     }
 }
