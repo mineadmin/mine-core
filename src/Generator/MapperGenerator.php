c4122949d8c930a01d7e8155e89e6c601ec491dc
nekgod <1559096467@qq.com>
2024-01-14 11:49:44 +0800

    mapper/handleSearch方法增加paramsEmptyQuery方法参数注入自动生成

--- src/Generator/MapperGenerator.php
+++ src/Generator/MapperGenerator.php
@@ -1,8 +1,7 @@
 <?php
-
 /** @noinspection PhpIllegalStringOffsetInspection */
-/* @noinspection PhpSignatureMismatchDuringInheritanceInspection */
-/*
+/** @noinspection PhpSignatureMismatchDuringInheritanceInspection */
+/**
  * MineAdmin is committed to providing solutions for quickly building web applications
  * Please view the LICENSE file that was distributed with this source code,
  * For the full copyright and license information.
@@ -13,41 +12,44 @@
  */
 
 declare(strict_types=1);
-/**
- * This file is part of MineAdmin.
- *
- * @link     https://www.mineadmin.com
- * @document https://doc.mineadmin.com
- * @contact  root@imoi.cn
- * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
- */
-
 namespace Mine\Generator;
 
 use App\Setting\Model\SettingGenerateColumns;
 use App\Setting\Model\SettingGenerateTables;
-use Hyperf\Support\Filesystem\Filesystem;
+use Mine\Interfaces\ServiceInterface\GenerateColumnServiceInterface;
+use Hyperf\Utils\Filesystem\Filesystem;
 use Mine\Exception\NormalStatusException;
 use Mine\Generator\Traits\MapperGeneratorTraits;
 use Mine\Helper\Str;
-use Mine\Interfaces\ServiceInterface\GenerateColumnServiceInterface;
 
 /**
  * Mapper类生成
- * Class MapperGenerator.
+ * Class MapperGenerator
+ * @package Mine\Generator
  */
 class MapperGenerator extends MineGenerator implements CodeGenerator
 {
     use MapperGeneratorTraits;
 
+    /**
+     * @var SettingGenerateTables
+     */
     protected SettingGenerateTables $model;
 
+    /**
+     * @var string
+     */
     protected string $codeContent;
 
+    /**
+     * @var Filesystem
+     */
     protected Filesystem $filesystem;
 
     /**
-     * 设置生成信息.
+     * 设置生成信息
+     * @param SettingGenerateTables $model
+     * @return MapperGenerator
      * @throws \Psr\Container\ContainerExceptionInterface
      * @throws \Psr\Container\NotFoundExceptionInterface
      */
@@ -86,7 +88,8 @@ class MapperGenerator extends MineGenerator implements CodeGenerator
     }
 
     /**
-     * 获取生成的类型.
+     * 获取生成的类型
+     * @return string
      */
     public function getType(): string
     {
@@ -94,39 +97,17 @@ class MapperGenerator extends MineGenerator implements CodeGenerator
     }
 
     /**
-     * 获取业务名称.
-     */
-    public function getBusinessName(): string
-    {
-        return Str::studly(str_replace(env('DB_PREFIX'), '', $this->model->table_name));
-    }
-
-    /**
-     * 设置代码内容.
-     */
-    public function setCodeContent(string $content)
-    {
-        $this->codeContent = $content;
-    }
-
-    /**
-     * 获取代码内容.
-     */
-    public function getCodeContent(): string
-    {
-        return $this->codeContent;
-    }
-
-    /**
      * 获取模板地址
+     * @return string
      */
     protected function getTemplatePath(): string
     {
-        return $this->getStubDir() . $this->getType() . '/mapper.stub';
+        return $this->getStubDir().$this->getType().'/mapper.stub';
     }
 
     /**
-     * 读取模板内容.
+     * 读取模板内容
+     * @return string
      */
     protected function readTemplate(): string
     {
@@ -134,7 +115,7 @@ class MapperGenerator extends MineGenerator implements CodeGenerator
     }
 
     /**
-     * 占位符替换.
+     * 占位符替换
      */
     protected function placeholderReplace(): MapperGenerator
     {
@@ -148,7 +129,7 @@ class MapperGenerator extends MineGenerator implements CodeGenerator
     }
 
     /**
-     * 获取要替换的占位符.
+     * 获取要替换的占位符
      */
     protected function getPlaceHolderContent(): array
     {
@@ -161,12 +142,12 @@ class MapperGenerator extends MineGenerator implements CodeGenerator
             '{FIELD_ID}',
             '{FIELD_PID}',
             '{FIELD_NAME}',
-            '{SEARCH}',
+            '{SEARCH}'
         ];
     }
 
     /**
-     * 获取要替换占位符的内容.
+     * 获取要替换占位符的内容
      */
     protected function getReplaceContent(): array
     {
@@ -179,28 +160,31 @@ class MapperGenerator extends MineGenerator implements CodeGenerator
             $this->getFieldIdName(),
             $this->getFieldPidName(),
             $this->getFieldName(),
-            $this->getSearch(),
+            $this->getSearch()
         ];
     }
 
     /**
-     * 初始化服务类命名空间.
+     * 初始化服务类命名空间
+     * @return string
      */
     protected function initNamespace(): string
     {
-        return $this->getNamespace() . '\\Mapper';
+        return $this->getNamespace() . "\\Mapper";
     }
 
     /**
-     * 获取控制器注释.
+     * 获取控制器注释
+     * @return string
      */
     protected function getComment(): string
     {
-        return $this->model->menu_name . 'Mapper类';
+        return $this->model->menu_name. 'Mapper类';
     }
 
     /**
-     * 获取使用的类命名空间.
+     * 获取使用的类命名空间
+     * @return string
      */
     protected function getUse(): string
     {
@@ -210,15 +194,17 @@ UseNamespace;
     }
 
     /**
-     * 获取类名称.
+     * 获取类名称
+     * @return string
      */
     protected function getClassName(): string
     {
-        return $this->getBusinessName() . 'Mapper';
+        return $this->getBusinessName().'Mapper';
     }
 
     /**
-     * 获取Model类名称.
+     * 获取Model类名称
+     * @return string
      */
     protected function getModelName(): string
     {
@@ -226,49 +212,56 @@ UseNamespace;
     }
 
     /**
-     * 获取树表ID.
+     * 获取树表ID
+     * @return string
      */
     protected function getFieldIdName(): string
     {
         if ($this->getType() == 'Tree') {
-            if ($this->model->options['tree_id'] ?? false) {
+            if ( $this->model->options['tree_id'] ?? false ) {
                 return $this->model->options['tree_id'];
+            } else {
+                return 'id';
             }
-            return 'id';
         }
         return '';
     }
 
     /**
-     * 获取树表父ID.
+     * 获取树表父ID
+     * @return string
      */
     protected function getFieldPidName(): string
     {
         if ($this->getType() == 'Tree') {
-            if ($this->model->options['tree_pid'] ?? false) {
+            if ( $this->model->options['tree_pid'] ?? false ) {
                 return $this->model->options['tree_pid'];
+            } else {
+                return 'parent_id';
             }
-            return 'parent_id';
         }
         return '';
     }
 
     /**
-     * 获取树表显示名称.
+     * 获取树表显示名称
+     * @return string
      */
     protected function getFieldName(): string
     {
         if ($this->getType() == 'Tree') {
-            if ($this->model->options['tree_name'] ?? false) {
+            if ( $this->model->options['tree_name'] ?? false ) {
                 return $this->model->options['tree_name'];
+            } else {
+                return 'name';
             }
-            return 'name';
         }
         return '';
     }
 
     /**
-     * 获取搜索内容.
+     * 获取搜索内容
+     * @return string
      */
     protected function getSearch(): string
     {
@@ -283,7 +276,36 @@ UseNamespace;
         foreach ($columns as $column) {
             $phpContent .= $this->getSearchCode($column);
         }
-
+        $phpContent = sprintf('$this->paramsEmptyQuery($params, [%s        ], $query);', PHP_EOL.$phpContent."\t".PHP_EOL);
         return $phpContent;
     }
+
+    /**
+     * 获取业务名称
+     * @return string
+     */
+    public function getBusinessName(): string
+    {
+        return Str::studly(str_replace(env('DB_PREFIX'), '', $this->model->table_name));
+    }
+
+
+    /**
+     * 设置代码内容
+     * @param string $content
+     */
+    public function setCodeContent(string $content)
+    {
+        $this->codeContent = $content;
+    }
+
+    /**
+     * 获取代码内容
+     * @return string
+     */
+    public function getCodeContent(): string
+    {
+        return $this->codeContent;
+    }
+
 }
--- src/Generator/Traits/MapperGeneratorTraits.php
+++ src/Generator/Traits/MapperGeneratorTraits.php
@@ -10,14 +10,6 @@
  */
 
 declare(strict_types=1);
-/**
- * This file is part of MineAdmin.
- *
- * @link     https://www.mineadmin.com
- * @document https://doc.mineadmin.com
- * @contact  root@imoi.cn
- * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
- */
 
 namespace Mine\Generator\Traits;
 
@@ -25,79 +17,37 @@ trait MapperGeneratorTraits
 {
     /**
      * 获取搜索代码
+     * @param $column
+     * @return string
      */
     protected function getSearchCode($column): string
     {
         return match ($column['query_type']) {
-            'neq' => $this->getSearchPHPString($column['column_name'], '!=', $column['column_comment']),
-            'gt' => $this->getSearchPHPString($column['column_name'], '<', $column['column_comment']),
-            'gte' => $this->getSearchPHPString($column['column_name'], '<=', $column['column_comment']),
-            'lt' => $this->getSearchPHPString($column['column_name'], '>', $column['column_comment']),
-            'lte' => $this->getSearchPHPString($column['column_name'], '>=', $column['column_comment']),
-            'like' => $this->getSearchPHPString($column['column_name'], 'like', $column['column_comment']),
+            'neq'     => $this->getSearchPHPString($column['column_name'], '!=', $column['column_comment']),
+            'gt'      => $this->getSearchPHPString($column['column_name'], '<', $column['column_comment']),
+            'gte'     => $this->getSearchPHPString($column['column_name'], '<=', $column['column_comment']),
+            'lt'      => $this->getSearchPHPString($column['column_name'], '>', $column['column_comment']),
+            'lte'     => $this->getSearchPHPString($column['column_name'], '>=', $column['column_comment']),
+            'like'    => $this->getSearchPHPString($column['column_name'], 'like', $column['column_comment']),
             'between' => $this->getSearchPHPString($column['column_name'], 'between', $column['column_comment']),
-            'in' => $this->getSearchPHPString($column['column_name'], 'in', $column['column_comment']),
-            'notin' => $this->getSearchPHPString($column['column_name'], 'notin', $column['column_comment']),
-            default => $this->getSearchPHPString($column['column_name'], '=', $column['column_comment']),
+            'in'      => $this->getSearchPHPString($column['column_name'], 'in', $column['column_comment']),
+            'notin'   => $this->getSearchPHPString($column['column_name'], 'notin', $column['column_comment']),
+            default   => $this->getSearchPHPString($column['column_name'], '=', $column['column_comment']),
         };
     }
 
+    /**
+     * @param $name
+     * @param $mark
+     * @param $comment
+     * @return string
+     */
     protected function getSearchPHPString($name, $mark, $comment): string
     {
-        if ($mark == 'like') {
-            return <<<php
+        return <<<PHP
+            // {$comment}
+            '{$name}' => '{$mark}',
 
-        // {$comment}
-        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
-            \$query->where('{$name}', 'like', '%'.\$params['{$name}'].'%');
-        }
-
-php;
-        }
-
-        if ($mark == 'between') {
-            return <<<php
-
-        // {$comment}
-        if (isset(\$params['{$name}']) && filled(\$params['{$name}']) && is_array(\$params['{$name}']) && count(\$params['{$name}']) == 2) {
-            \$query->whereBetween(
-                '{$name}',
-                [ \$params['{$name}'][0], \$params['{$name}'][1] ]
-            );
-        }
-
-php;
-        }
-
-        if ($mark == 'in') {
-            return <<<php
-
-        // {$comment}
-        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
-            \$query->whereIn('{$name}', \$params['{$name}']);
-        }
-
-php;
-        }
-
-        if ($mark == 'notin') {
-            return <<<php
-
-        // {$comment}
-        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
-            \$query->whereNotIn('{$name}', \$params['{$name}']);
-        }
-
-php;
-        }
-
-        return <<<php
-
-        // {$comment}
-        if (isset(\$params['{$name}']) && filled(\$params['{$name}'])) {
-            \$query->where('{$name}', '{$mark}', \$params['{$name}']);
-        }
-
-php;
+PHP;
     } // 该方法结束位置
 }
