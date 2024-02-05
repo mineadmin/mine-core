e31fa5d0c048a4fa6e0e5bd0b76422489507349f
X.Mo <261091613@qq.com>
2024-02-05 18:09:42 +0800

    Update MapperTrait.php

--- src/Traits/MapperTrait.php
+++ src/Traits/MapperTrait.php
@@ -308,10 +308,6 @@ trait MapperTrait
     public function delete(array $ids): bool
     {
         $this->model::destroy($ids);
-
-        $manager = ApplicationContext::getContainer()->get(Manager::class);
-        $manager->destroy($ids, $this->model);
-
         return true;
     }
 
